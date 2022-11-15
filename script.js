171d4a0c3af25d89f8b2c5b48e57363d2151c378
jeyakumarjegatheesan <115686336+jeyakumarjegatheesan@users.noreply.github.com>
2022-11-15 17:39:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+
+setTimeout(()=>{
+    var count1=document.createElement("p")
+    console.log("10");
+    count1.innerText=10
+    count1.style.color="red"
+    count1.style.fontSize="50px"
+    document.body.append(count1);
+    count1.style.backgroundColor="yellow"
+
+},1000)
+
+setTimeout(()=>{
+    var count2=document.createElement("p")
+    console.log("9");
+    count2.innerText=9
+    count2.style.color="blue"
+    count2.style.fontSize="40px"
+    document.body.append(count2);
+    count2.style.backgroundColor="red"
+
+},2000)
+
+setTimeout(()=>{
+    var count3=document.createElement("p")
+    console.log("8");
+    count3.innerText=8
+    count3.style.color="pink"
+    count3.style.fontSize="35px"
+    document.body.append(count3);
+    count3.style.backgroundColor="blue"
+
+},3000)
+
+setTimeout(()=>{
+    var count4=document.createElement("p")
+    console.log("7");
+    count4.innerText=7
+    count4.style.color="green"
+    count4.style.fontSize="30px"
+    document.body.append(count4);
+    count4.style.backgroundColor="pink"
+
+},4000)
+
+
+setTimeout(()=>{
+    var count5=document.createElement("p")
+    console.log("6");
+    count5.innerText=6
+    count5.style.color="lightblue"
+    count5.style.fontSize="15px"
+    document.body.append(count5);
+    count5.style.backgroundColor="red"
+
+},5000)
+
+
+setTimeout(()=>{
+    var count6=document.createElement("p")
+    console.log("5");
+    count6.innerText=5
+    count6.style.color="red"
+    count6.style.fontSize="15px"
+    document.body.append(count6);
+    count6.style.backgroundColor="pink"
+
+},6000)
+
+setTimeout(()=>{
+    var count7=document.createElement("p")
+    console.log("4");
+    count7.innerText=4
+    count7.style.color="green"
+    count7.style.fontSize="20px"
+    document.body.append(count7);
+    count7.style.backgroundColor="grey"
+
+},7000)
+
+
+setTimeout(()=>{
+    var count8=document.createElement("p")
+    console.log("3");
+    count8.innerText=3
+    count8.style.color="red"
+    count8.style.fontSize="25px"
+    document.body.append(count8);
+    count8.style.backgroundColor="yellow"
+
+},8000)
+
+setTimeout(()=>{
+    var count9=document.createElement("p")
+    console.log("2");
+    count9.innerText=2
+    count9.style.color="blue"
+    count9.style.fontSize="30px"
+    document.body.append(count9);
+    count9.style.backgroundColor="lightgreen"
+
+},9000)
+
+setTimeout(()=>{
+    var count10=document.createElement("p")
+    console.log("1");
+    count10.innerText=1
+    count10.style.color="red"
+    count10.style.fontSize="35px"
+    document.body.append(count10);
+    count10.style.backgroundColor="yellow"
+
+},10000)
+
+
+setTimeout(()=>{
+    var count11=document.createElement("p")
+    console.log("Happy Independence Day");
+    count11.innerText="Happy Independence Day"
+    count11.style.color="green"
+    count11.style.fontSize="50px"
+    document.body.append(count11);
+    count11.style.backgroundColor="pink"
+
+},11000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
